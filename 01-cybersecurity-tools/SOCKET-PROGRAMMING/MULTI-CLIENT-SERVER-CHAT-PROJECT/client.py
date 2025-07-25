109f698f18e66b3a18d17f6042d6cb3da629c68e
Dipanshu batra <devbatra887@gmail.com>
2025-07-25 21:08:45 +0530

    Create client.py

---
+++ 01-cybersecurity-tools/SOCKET-PROGRAMMING/MULTI-CLIENT-SERVER-CHAT-PROJECT/client.py
@@ -0,0 +1,30 @@
+import socket as s
+import threading
+
+def recive():
+    while True:
+        try:
+            msg = client.recv(1024).decode()
+            print('\r' + msg + '\nYou: ', end="")
+        except:
+            print("[error] disconnected")
+            break
+
+client = s.socket(s.AF_INET, s.SOCK_STREAM)
+client.connect(("127.0.0.1", 9999))
+
+print(client.recv(1024).decode().strip(), end='')
+name = input("")
+client.send(name.encode())
+
+thread = threading.Thread(target=recive, daemon=True)
+thread.start()
+
+while True:
+    mesg = input("You: ")
+    if mesg.lower() == 'exit':
+        client.send(f'{name} has left the chat'.encode())
+        break
+    client.send(f'{name} :- {mesg}'.encode())
+
+client.close()
